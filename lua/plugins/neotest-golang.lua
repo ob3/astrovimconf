818193aee7d385a220e6f936ca58f1ce7a17ee35
edo <andrew.karsten@gojek.com>
2025-10-02 00:59:28 +0700

    use v1 for neotest-golang

--- lua/plugins/neotest-golang.lua
+++ lua/plugins/neotest-golang.lua
@@ -5,22 +5,37 @@ return {
       "nvim-neotest/nvim-nio",
       "nvim-lua/plenary.nvim",
       "antoinemadec/FixCursorHold.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      { "fredrikaverpil/neotest-golang", version = "*" }, -- Installation
-      { "andythigpen/nvim-coverage", version = "*" },
+      {
+        "nvim-treesitter/nvim-treesitter", -- Optional, but recommended
+        branch = "main", -- NOTE; not the master branch!
+        build = function() vim.cmd ":TSUpdate go" end,
+      },
+      {
+        "fredrikaverpil/neotest-golang",
+        -- dependencies = {
+        --   "andythigpen/nvim-coverage", -- Added dependency
+        -- },
+        version = "v1.15.1", -- Optional, but recommended; track releases
+        build = function()
+          vim.system({ "go", "install", "gotest.tools/gotestsum@latest" }):wait() -- Optional, but recommended
+        end,
+      },
     },
     config = function()
-      require("coverage").setup()
-
-      local neotest_golang_opts = {
-        -- testify_enabled = true,
-        -- runner = "gotestsum",
-        go_test_args = { "-cover", "-v" },
-      } -- Specify custom configuration
-
+      local config = {
+        runner = "gotestsum", -- Optional, but recommended
+        -- gotestsum_args = {
+        --   "-v",
+        --   "-race",
+        --   "-count=1",
+        --   "-- -coverprofile=coverage.out",
+        --   "-coverpkg=./...",
+        -- },
+        testify_enabled = true,
+      }
       require("neotest").setup {
         adapters = {
-          require "neotest-golang"(neotest_golang_opts), -- Registration
+          require "neotest-golang"(config),
         },
       }
     end,
